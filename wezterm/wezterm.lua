19c059dac4e78eed08c64856f15ebabb3d5ca592
AR <115971126+alifR999@users.noreply.github.com>
2025-06-06 11:24:51 +0800

    new adjustments

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,14 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.initial_cols = 120
-config.initial_rows = 28
+config.initial_cols = 160
+config.initial_rows = 40
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 config.font_size = 14
 
