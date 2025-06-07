bb14cb81673e53f6d208ff906c6cea5d498d1e79
a454-dev <74300892+a454-dev@users.noreply.github.com>
2025-06-07 12:28:36 +0800

    minimal (#15)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,11 @@
 local wezterm = require("wezterm")
+
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
 local config = wezterm.config_builder()
 
 config.initial_cols = 160
@@ -15,7 +22,7 @@ config.font_size = 14
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
-config.color_scheme = "AdventureTime"
+config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("JetBrains Mono")
-
+config.native_macos_fullscreen_mode = true
 return config
